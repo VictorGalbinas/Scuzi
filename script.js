db25f0a0df63ca00605f2670cd484c3851489bf1
VictorGalbinas <galbinasv1981@gmail.com>
2026-01-04 23:19:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function movePage(e, page) {
     currentPage += 2;
     toggleClass(e, "left-side");
     toggleClass(e.nextElementSibling, "left-side");
-  } else if ((page = currentPage - 1)) {
+  } else if (page == currentPage - 1) { // AICI era eroarea, trebuie "==" în loc de "="
     currentPage -= 2;
     toggleClass(e, "left-side");
     toggleClass(e.previousElementSibling, "left-side");
